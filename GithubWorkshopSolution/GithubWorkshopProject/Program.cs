931250abaaa843349b7a52684ea72ef0a8767a51
DevBauke <boef.bosma@gmail.com>
2025-02-14 11:00:20 +0100

    Update Program.cs

--- GithubWorkshopSolution/GithubWorkshopProject/Program.cs
+++ GithubWorkshopSolution/GithubWorkshopProject/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Start();
+            
+        }
+
+        public void Start()
+        {
             Console.WriteLine("Hello, World!");
         }
     }
